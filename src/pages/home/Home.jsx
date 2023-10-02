ddfe0de8b21851a6c386dfe86a7873b56836e15d
Yusuf <baliyusuf675@gmail.com>
2023-10-02 22:39:03 +0300

    v5

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+                <strong className="main-name"> Yusuf Balı</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
