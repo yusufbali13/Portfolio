83bf515138c968eeb0944761bfe997fcdc458daf
Yusuf <baliyusuf675@gmail.com>
2023-10-12 22:22:02 +0300

    v2

--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -13,12 +13,12 @@ function Resume() {
   return (
     <div className="flex items-center justify-center flex-col relative">
       <img
-        src="../../../Front End Resume V3.pdf"
+        src="../../../cw.pdf"
         alt=""
         className="h-full w-full object-cover opacity-10 absolute bottom-0"
       />
       <a
-        href="../../../Front End Resume V3.pdf"
+        href="../../../cw.pdf"
         target="_blank"
         rel="noreferrer"
         className="flex items-center z-20 justify-center gap-3 px-6 py-2 rounded  shadow-md text-white bg-purple-600 hover:bg-purple-700 transition duration-300 ease-in-out my-3"
@@ -27,14 +27,14 @@ function Resume() {
       </a>
       <div className="py-[50px] justify-center flex items-center overflow-hidden z-20">
         <Document
-          file="../../../Front End Resume V3.pdf"
+          file="../../../cw.pdf"
           className="flex justify-center shadow-xl"
         >
           <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
         </Document>
       </div>
       <a
-        href="../../../Front End Resume V3.pdf"
+        href="../../../cw.pdf"
         target="_blank"
         rel="noreferrer"
         className="flex items-center z-20 justify-center gap-3 px-6 py-2 rounded  shadow-md text-white bg-purple-600 hover:bg-purple-700 transition duration-300 ease-in-out my-3"
